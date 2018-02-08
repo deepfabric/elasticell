506850d33358940ce823477eef36a20de5cfd696
Zhichang Yu <yuzhichang@gmail.com>
2018-02-08 16:07:39 +0800

    improved debug.sh

--- debug.sh
+++ debug.sh
@@ -8,16 +8,22 @@ CFG_DIR=$ELASTICELL_DIR/cfg
 LOG_DIR=$ELASTICELL_DIR/log
 
 start_elasticell_pd() {
-    start-stop-daemon --start --oknodo --background --make-pidfile --pidfile $ELASTICELL_DIR/pd$1.pid --startas $ELASTICELL_DIR/pd -- --name=pd$1 --data=$ELASTICELL_DIR/pd$1/data --addr-rpc=:2080$1 --urls-client=http://127.0.0.1:237$1 --urls-peer=http://127.0.0.1:238$1 --initial-cluster=pd1=http://127.0.0.1:2381,pd2=http://127.0.0.1:2382,pd3=http://127.0.0.1:2383 --log-file=$LOG_DIR/pd$1.log --log-level=debug
+    set -x
+    start-stop-daemon --start --oknodo --background --make-pidfile --pidfile $ELASTICELL_DIR/pd$1.pid --startas $ELASTICELL_DIR/pd -- --name=pd$1 --data=$ELASTICELL_DIR/pd$1/data --addr-rpc=:2080$1 --urls-client=http://127.0.0.1:237$1 --urls-peer=http://127.0.0.1:238$1 --initial-cluster=pd1=http://127.0.0.1:2381,pd2=http://127.0.0.1:2382,pd3=http://127.0.0.1:2383 --log-file=$LOG_DIR/pd$1.log --log-level=info
+    set +x
 }
 
 start_elasticell_cell() {
-    start-stop-daemon --start --oknodo --background --make-pidfile --pidfile $ELASTICELL_DIR/cell$1.pid --startas $ELASTICELL_DIR/cell -- --pd=127.0.0.1:20801,127.0.0.1:20802,127.0.0.1:20803 --addr=127.0.0.1:1080$1 --addr-cli=:637$1 --zone=zone-$1 --rack=rack-$1 --data=$ELASTICELL_DIR/cell$1/data --interval-heartbeat-store=5 --interval-heartbeat-cell=2 --addr-pprof=:638$1 --metric-job=appid_cell --metric-instance=cell$1 --metric-address=127.0.0.1:9091 --interval-metric-sync=5 --log-file=$LOG_DIR/cell$1.log --log-level=debug
+    set -x
+    start-stop-daemon --start --oknodo --background --make-pidfile --pidfile $ELASTICELL_DIR/cell$1.pid --startas $ELASTICELL_DIR/cell -- --pd=127.0.0.1:20801,127.0.0.1:20802,127.0.0.1:20803 --addr=127.0.0.1:1080$1 --addr-cli=:637$1 --zone=zone-$1 --rack=rack-$1 --data=$ELASTICELL_DIR/cell$1/data --interval-heartbeat-store=5 --interval-heartbeat-cell=2 --addr-pprof=:638$1 --metric-job=appid_cell --metric-instance=cell$1 --metric-address=127.0.0.1:9091 --interval-metric-sync=5 --log-file=$LOG_DIR/cell$1.log --log-level=info
+    set +x
 }
 
 start_elasticell_proxy() {
+    set -x
 #    start-stop-daemon --start --oknodo --background --make-pidfile --pidfile $ELASTICELL_DIR/proxy.pid --startas /bin/bash -- -c "exec $ELASTICELL_DIR/redis-proxy --cfg=$CFG_DIR/proxy.json --log-level=debug > $LOG_DIR/proxy.log 2>&1"
-    start-stop-daemon --start --oknodo --background --make-pidfile --pidfile $ELASTICELL_DIR/proxy.pid --startas $ELASTICELL_DIR/redis-proxy -- --cfg=$CFG_DIR/proxy.json --log-file=$LOG_DIR/proxy.log --log-level=debug
+    start-stop-daemon --start --oknodo --background --make-pidfile --pidfile $ELASTICELL_DIR/proxy.pid --startas $ELASTICELL_DIR/redis-proxy -- --cfg=$CFG_DIR/proxy.json --log-file=$LOG_DIR/proxy.log --log-level=info
+    set +x
 }
 
 stop_elasticell() {
