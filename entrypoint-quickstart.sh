860be9455a687be572990d7467d51d44b7877616
fagongzi <zhangxu19830126@gmail.com>
2018-12-31 20:45:04 +0800

    dev: adjust quick start dockerfile

--- entrypoint-quickstart.sh
+++ entrypoint-quickstart.sh
@@ -5,15 +5,15 @@ CFG_DIR=$ELASTICELL_DIR/cfg
 LOG_DIR=$ELASTICELL_DIR/log
 
 start_elasticell_pd() {
-    $ELASTICELL_DIR/pd --log-file=$LOG_DIR/pd$1.log --name=pd$1 --data=/apps/deepfabric/pd$1/data --addr-rpc=:2080$1 --urls-client=http://127.0.0.1:237$1 --urls-peer=http://127.0.0.1:238$1 --initial-cluster=pd1=http://127.0.0.1:2381,pd2=http://127.0.0.1:2382,pd3=http://127.0.0.1:2383 &
+    pd --log-file=$LOG_DIR/pd$1.log --name=pd$1 --data=/apps/deepfabric/pd$1/data --addr-rpc=:2080$1 --urls-client=http://127.0.0.1:237$1 --urls-peer=http://127.0.0.1:238$1 --initial-cluster=pd1=http://127.0.0.1:2381,pd2=http://127.0.0.1:2382,pd3=http://127.0.0.1:2383 &
 }
 
 start_elasticell_cell() {
-    $ELASTICELL_DIR/cell --log-file=$LOG_DIR/cell$1.log --pd=127.0.0.1:20801,127.0.0.1:20802,127.0.0.1:20803 --addr=127.0.0.1:1080$1 --addr-cli=:637$1 --zone=zone-$1 --rack=rack-$1 --data=/apps/deepfabric/cell$1/data --interval-heartbeat-store=5 --interval-heartbeat-cell=2 &
+    cell --log-file=$LOG_DIR/cell$1.log --pd=127.0.0.1:20801,127.0.0.1:20802,127.0.0.1:20803 --addr=127.0.0.1:1080$1 --addr-cli=:637$1 --zone=zone-$1 --rack=rack-$1 --data=/apps/deepfabric/cell$1/data --interval-heartbeat-store=5 --interval-heartbeat-cell=2 &
 }
 
 start_elasticell_proxy() {
-    $ELASTICELL_DIR/redis-proxy --cfg=$CFG_DIR/proxy.json
+    redis-proxy --cfg=$CFG_DIR/proxy.json
 }
 
 echo "begin to start pd cluster"
